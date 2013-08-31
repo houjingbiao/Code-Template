713a6b055ad907cc4fec319b63c1382618c43a31
houjingbiao <Jingzhaohu@163.com>
2013-08-31 12:01:53 -0700

    -finish 1026

--- 000wikioi_130816.cpp
+++ 000wikioi_130816.cpp
@@ -1,3 +1,190 @@
+//1026 逃跑的拉尔夫
+#include <stdio.h>
+#include <string.h>
+#include <queue>
+#include <utility>
+#include <vector>
+using namespace std;
+#define MAXR 51
+#define MAXC 51
+char a[MAXR][MAXC];
+int main(){
+	freopen("in.txt", "r", stdin);
+	int R, C;
+	int posx, posy;
+	scanf("%d%d", &R, &C);
+	for(int i = 0; i < R; i++){
+		char xx[MAXC];
+		scanf("%s", &xx);
+		for(int j = 0; j < C; j++){
+			a[i][j] = xx[j];
+			if(a[i][j] == '*'){
+				posx = i;
+				posy = j;
+				a[i][j] = '.';
+			}
+		}
+	}
+	int N;
+	scanf("%d", &N);
+	int step = 0;
+	queue<pair<int, int> > q1;
+	q1.push(pair<int, int>(posx, posy));
+	while(step < N){
+		char s[10];
+		scanf("%s", &s);
+		int diffx = -1;
+		int diffy = 0;
+		switch(s[0]){
+		case 'N':
+			diffx = -1;
+			diffy = 0;
+			break;
+		case 'S':
+			diffx = 1;
+			diffy = 0;
+			break;
+		case 'W':
+			diffx = 0;
+			diffy = -1;
+			break;
+		case 'E':
+		default:
+			diffx = 0;
+			diffy = 1;
+			break;
+		}
+		queue<pair<int, int> > q2;
+		while(!q1.empty()){
+			posx = q1.front().first;
+			posy = q1.front().second;
+			q1.pop();
+			posx = posx + diffx;
+			posy = posy + diffy;
+			while(posx >= 0 && posx < R && posy >= 0 && posy < C && a[posx][posy] != 'X'){
+				q2.push(pair<int, int>(posx, posy));
+				posx = posx + diffx;
+				posy = posy + diffy;
+			}
+		}
+		q1 = q2;
+		step++;
+	}
+	while(!q1.empty()){
+		posx = q1.front().first;
+		posy = q1.front().second;
+		q1.pop();
+		a[posx][posy] = '*';
+	}
+	for(int i = 0; i < R; i++){
+		for(int j = 0; j < C; j++)
+			printf("%c ", a[i][j]);
+		printf("\n");
+	}
+	return 0;
+}
+
+
+//version: dfs
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+const int MAX_N = 51;
+const int MAX_M = 1001;
+int R, C, N;
+int x, y;
+int turn[MAX_M]; //移动方向 ，turn[t] 表示 第t个方向
+int M[MAX_N][MAX_N]; //以0，1存储原地图，方便判断
+char A[MAX_N][MAX_N]; //以字符存储行驶后的地图，方便输出
+int vis[MAX_N][MAX_N][MAX_M]; //vis[x][y][t] 表示从点（x,y）往第t个方向（turn[t]）移动
+void DFS(int x, int y, int t)
+{
+	if (vis[x][y][t]) return; //如果该移动状态出现过了，则不考虑
+	vis[x][y][t] = 1; //没出现过，则考虑，标记。。
+	if (t > N) //如果遍历完所有行驶方向，则得到一个目标可能点。
+	{
+		A[x][y] = '*';
+		return;
+	}
+	if (turn[t] == 1) //北（上）
+	{
+		x--; //向上移动一步
+		while (x >= 1 && M[x][y]) //约束条件，一直行驶直到无法继续
+		{
+			DFS(x, y, t + 1); //从当前点开始搜索，按照turn的顺序行驶，每个方向都要走到边界
+			x--;
+		}
+	}
+	if (turn[t] == 2) //南（下）
+	{
+		x++;
+		while (x <= R && M[x][y])
+		{
+			DFS(x, y, t+1);
+			x++;
+		}
+	}
+	if (turn[t] == 3) //西（左）
+	{
+		y--;
+		while (y >= 1 && M[x][y])
+		{
+			DFS(x, y, t + 1);
+			y--;
+		}
+	}
+	if (turn[t] == 4) //东（右）
+	{
+		y++;
+		while (y <= C && M[x][y])
+		{
+			DFS(x, y, t + 1);
+			y++;
+		}
+	}
+}
+int main()
+{
+	int i, j;
+	cin>>R>>C;
+	for (i = 1; i <= R; i++)
+	{
+		for (j = 1; j <= C; j++)
+		{
+			cin>>A[i][j];
+			if (A[i][j] == '.')M[i][j] = 1;
+			if (A[i][j] == 'X')M[i][j] = 0;
+			if (A[i][j] == '*')
+			{
+				A[i][j] = '.';
+				M[i][j] = 1;
+				x = i;
+				y = j;
+			}
+		}
+	}
+	cin>>N;
+	for (i = 1; i <= N; i++)
+	{
+		char dir[5];
+		cin>>dir;
+		if (dir[0] == 'N') turn[i] = 1;
+		if (dir[0] == 'S') turn[i] = 2;
+		if (dir[0] == 'W') turn[i] = 3;
+		if (dir[0] == 'E') turn[i] = 4;
+	}
+	DFS(x, y, 1);
+	for (i = 1; i <= R; i++)
+	{
+		for (j = 1; j <= C; j++)
+			printf("%c", A[i][j]);
+		if(i != R)
+			printf("\n");
+	}
+	return 0;
+}
+
+
 //1004 四子连棋
 #include <stdio.h>
 #include <string.h>
