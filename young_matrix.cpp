88ed755318faa44e64ebe7ee7bf4c8ebad63ddf1
houjingbiao <Jingzhaohu@163.com>
2013-09-19 00:23:47 -0700

    -update young_matrix.cpp

--- young_matrix.cpp
+++ young_matrix.cpp
@@ -1,3 +1,11 @@
+//young矩阵可以认为是以为排序数组的推广，并且与堆（heap）有很大的相似。
+//增删查改：
+//在一位排序数组里面，任意两个相邻的元素之间的大小关系是确定的。而在young矩阵里面，任意一个2*2的子矩阵[A(i,j), A(i,j+1); A(i+1,j), A(i+1,j+1)]都满足：A(i,j) < A(i,j+1), A(i+1,j) < A(i+1,j+1)。在堆里面，是把数组中的元素组织成了一个完全二叉树，且满足A[i] < A[2*i], A[i] < A[2*i+1]。
+//另外，从递归的角度看的话，三者都是递归的结构。
+//增：从右下角开始
+//删：1.删除左上角的最小元素， 2.删除任意元素
+//查：查找的过程其实是相当于一个剪枝的过程，具体地，比较查找值与中间节点，比较结果来确定下一步的查找范围。观察young矩阵可以知道每一个7型都是一个
+//改：
 #include "stdafx.h"
 #include <string>
 #include <vector>
@@ -7,9 +15,7 @@ using namespace std;
 #define MAXV 0x7FFFFFFF
 #define MAX_M 10
 #define MAX_N 10
-
 int Y[MAX_M][MAX_N];
-
 void young_reset(){
 	for(int i = 0; i < MAX_M; i++)
 		for(int j = 0; j < MAX_N; j++)
@@ -89,40 +95,6 @@ bool young_find(int val){
 	return false;
 }
 
-
-
-void young_insert_rec(int val, int i, int j){
-	/*if(val >= MAXV)	return;
-	int i = 0, j = MAX_N - 1;
-	while(j>=0 && i < MAX_M){
-	if(val < Y[i][j])
-	j--;
-	else if(val > Y[i][j])
-	i++;
-	else
-	return true;
-	}*/
-
-	while(i < MAX_M && j < MAX_N){
-		if(val > Y[i][j]){
-			i++, j++;
-		}
-		else if(val < Y[i-1][j]){
-			int t = val;
-			val = Y[i-1][j];
-			Y[i-1][j] = t;
-			return young_insert_rec(val, i-1, j-1);
-		}
-	}
-	while(i == MAX_M && Y[i-1][j] < val){
-		j++;
-	}
-	while(j == MAX_N && Y[i][j-1] < val){
-		i++;
-	}
-	return;
-}
-
 void swap(int &a, int &b)
 {
 	int temp;
@@ -131,23 +103,20 @@ void swap(int &a, int &b)
 	b = temp;
 }
 
-//void young_min_heapify(int a[][11],int i,int j)
 void young_min_heapify(int i,int j)
 {
-	int x,y;
-	if (i>0 && Y[i][j] < Y[i-1][j])
-	{
+	int x = i， y = j;
+	if (i>0 && Y[i][j] < Y[i-1][j]){
 		x = i-1;
 		y = j;
 	}
-	else { x = i; y = j;}
-	if (j>0 && Y[x][y] < Y[i][j-1])
-	{
+	
+	if (j>0 && Y[x][y] < Y[i][j-1]){
 		x = i;
 		y = j-1;
 	}
-	if (x !=i || y !=j)
-	{
+	
+	if (x !=i || y !=j)	{
 		swap(Y[i][j],Y[x][y]);
 		printf("\n%d %d:\n", x, y);
 		young_print();
